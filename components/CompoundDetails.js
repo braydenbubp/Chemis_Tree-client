6bea9040750fbea4cbd18bab1d4004fe37402f02
braydenbubp <braydenbubp1@gmail.com>
2024-09-10 16:16:39 -0400

    del compound comp

--- components/CompoundDetails.js
+++ components/CompoundDetails.js
@@ -1,14 +1,24 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
-import { getSingleCompound } from '../api/compounds';
+import { useRouter } from 'next/router';
+import { Button } from 'react-bootstrap';
+import { deleteCompound, getSingleCompound } from '../api/compounds';
 
 export default function CompoundDetail({ compoundId }) {
   const [compound, setCompound] = useState(null);
+
+  const router = useRouter();
 
   const getOneCompound = () => {
     getSingleCompound(compoundId).then((data) => {
       setCompound(data);
     });
+  };
+
+  const deleteThisCompound = () => {
+    if (window.confirm(`Delete ${compound.molecular_formula}`)) {
+      deleteCompound(compound.id).then(() => router.push('/compounds'));
+    }
   };
 
   useEffect(() => {
@@ -19,10 +29,17 @@ export default function CompoundDetail({ compoundId }) {
   }, []);
 
   return (
-    <div>
-      <h1>{compound?.iupac_name}</h1>
-      <h1>{compound?.molecular_formula}</h1>
-      <h2>{compound?.molecular_weight}</h2>
-    </div>
+    <>
+      <div>
+        <h1>{compound?.iupac_name}</h1>
+        <h1>{compound?.molecular_formula}</h1>
+        <h2>{compound?.molecular_weight}</h2>
+      </div>
+      <>
+        <Button id="del-compound" onClick={deleteThisCompound}>
+          Delete
+        </Button>
+      </>
+    </>
   );
 }
