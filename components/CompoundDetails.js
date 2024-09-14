a4eb0300477be11ed77345f3f482637cb5f0e420
braydenbubp <braydenbubp1@gmail.com>
2024-09-14 10:09:06 -0400

    each compound displays pertinent info

--- components/CompoundDetails.js
+++ components/CompoundDetails.js
@@ -6,22 +6,62 @@ import Image from 'next/image';
 import { deleteCompound, getSingleCompound } from '../api/compounds';
 
 const API_BASE_URL = 'http://localhost:8000';
+const PUBCHEM_API_BASE = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug';
+const PUBCHEM_VIEW_API_BASE = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound';
 
 export default function CompoundDetail({ compoundId }) {
   const [compound, setCompound] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [cidData, setCidData] = useState(null);
+  const [description, setDescription] = useState('');
 
   const router = useRouter();
 
-  const getOneCompound = () => {
+  const getCidData = async (cid) => {
+    try {
+      const response = await fetch(`${PUBCHEM_API_BASE}/compound/cid/${cid}/JSON`);
+      if (!response.ok) {
+        throw new Error('failed to get data');
+      }
+      const data = await response.json();
+      setCidData(data.PC_Compounds[0]);
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  const getDescription = async (cid) => {
+    try {
+      const response = await fetch(`${PUBCHEM_VIEW_API_BASE}/${cid}/JSON`);
+      if (!response.ok) {
+        throw new Error('failed to get description');
+      }
+      const data = await response.json();
+      const descriptionSection = data.Record.Section.find((section) => section.TOCHeading === 'Names and Identifiers');
+      const descriptionRecord = descriptionSection?.Section.find((section) => section.TOCHeading === 'Record Description');
+      setDescription(descriptionRecord?.Information[0]?.Value?.StringWithMarkup[0]?.String || 'No description available.');
+    } catch (err) {
+      console.warn(err);
+      setDescription('Failed to load description.');
+    }
+  };
+
+  const getOneCompound = async () => {
     setLoading(true);
-    getSingleCompound(compoundId).then((data) => {
+    try {
+      const data = await getSingleCompound(compoundId);
       setCompound(data);
-      setLoading(false);
-    }).catch((error) => {
+      if (data.cid) {
+        await getCidData(data.cid);
+        await getDescription(data.cid);
+      } else {
+        console.warn('No CID available for this compound');
+      }
+    } catch (error) {
       console.error('Failed to fetch', error);
+    } finally {
       setLoading(false);
-    });
+    }
   };
 
   useEffect(() => {
@@ -29,7 +69,7 @@ export default function CompoundDetail({ compoundId }) {
       getOneCompound();
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [compoundId]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -44,7 +84,7 @@ export default function CompoundDetail({ compoundId }) {
       deleteCompound(compound.id).then(() => router.push('/compounds'));
     }
   };
-  console.warn(compound);
+
   const getImgSrc = () => {
     if (compound.model_2d_url) {
       return compound.model_2d_url.startsWith('http') ? compound.model_2d_url
@@ -76,7 +116,20 @@ export default function CompoundDetail({ compoundId }) {
           )}
 
         </div>
+        {compound?.info_link && (
+          <div className="linkInfo">
+            <h4>Information on this compound</h4>
+          </div>
+        )}
+        {cidData && (
+          <p><strong>InChI:</strong> {cidData.props.find((p) => p.urn.label === 'InChI')?.value?.sval}</p>
+        )}
+        {description && (
+          <p>{description}</p>
+        )}
+        <a href={compound?.info_link}>For more information go here!</a>
       </div>
+
       <>
         <Button id="del-compound" onClick={deleteThisCompound}>
           Delete
